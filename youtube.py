2446f10a7313d9286a47ed441de9902473692698
Shubham Gupta <114990707+its-shubham15m@users.noreply.github.com>
2023-10-24 02:10:46 +0530

    test

--- youtube.py
+++ youtube.py
@@ -2,44 +2,19 @@ import streamlit as st
 from pytube import YouTube
 from PIL import Image
 import requests
-from io import BytesIO
+import io
 
 st.set_page_config(
-    page_title="WebSave - YouTube",
-    page_icon=Image.open("downloading.png"),
+    page_title="YouTube Media Downloader",
+    page_icon=":arrow_down:",
     layout="wide"
 )
 
-# Set the Streamlit app title
-st.title("WebSave - Download YouTube Videos & Audios")
+st.title("YouTube Media Downloader")
 
-st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background: url("https://unsplash.com/photos/7PGqXW9k0ws/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NDN8fG11c2ljJTIwYmFuZHxlbnwwfHx8fDE2OTgwODc0MTJ8MA&force=true");
-        background-size: cover;
-        background-position: center;
-    }}
-    
-    .thumbnail-container {{
-        border-radius: 10px;
-        overflow: hidden;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-    }}
-
-    .thumbnail-image {{
-        width: 100%;
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# Input field for the YouTube URL
 url = st.text_input("Enter the YouTube URL:")
 
-@st.cache_data
+@st.cache
 def get_video_info(url):
     try:
         yt = YouTube(url)
@@ -51,10 +26,9 @@ def get_video_info(url):
 
 def resize_thumbnail(thumbnail_url):
     response = requests.get(thumbnail_url)
-    img = Image.open(BytesIO(response.content))
+    img = Image.open(io.BytesIO(response.content))
     return img
 
-# Check if a URL is provided
 if url:
     with st.spinner("Fetching video information..."):
         yt, thumbnail_url = get_video_info(url)
@@ -62,37 +36,28 @@ if url:
     if yt:
         st.subheader("Video Thumbnail:")
         thumbnail = resize_thumbnail(thumbnail_url)
-        # Apply the rounded border to the thumbnail
         st.image(thumbnail, use_column_width=True, caption="Video Thumbnail", output_format='JPEG', channels="BGR")
 
-        download_option = st.radio("Select download type:", ("Video", "Audio"))
+        download_format = st.radio("Select Download Format:", ["MP3 (Audio)", "MP4 (Video)"])
 
-        if download_option == "Video":
-            video_streams = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
-
-            # Generate a direct download link for the selected stream
-            stream_options = [f"{stream.resolution} - {stream.mime_type} - {stream.itag}" for stream in video_streams]
-            selected_stream_option = st.selectbox("Select a video stream to generate a direct download link:", stream_options)
-            if selected_stream_option:
-                selected_stream_index = stream_options.index(selected_stream_option)
-                selected_stream = video_streams[selected_stream_index]
-                download_url = selected_stream.url
-                st.subheader("Download Video:")
-                st.markdown(f'<a href="{download_url}" download>Click to Download</a>', unsafe_allow_html=True)
-
-        if download_option == "Audio":
-            audio_streams = yt.streams.filter(only_audio=True, file_extension='mp4')
-
-            # Generate audio quality choices dynamically
-            audio_quality_choices = [f"{audio_stream.abr.replace('kbps', '')}kbps" for audio_stream in audio_streams]
-            audio_quality = st.selectbox("Select audio quality:", audio_quality_choices)
-            selected_audio_stream = next((audio_stream for audio_stream in audio_streams if audio_quality in audio_stream.abr), None)
-            if selected_audio_stream:
-                download_url = selected_audio_stream.url
-                st.subheader("Download Audio:")
-                st.markdown(f'<a href="{download_url}" download>Click to Download</a>', unsafe_allow_html=True)
-            else:
-                st.warning("No audio stream available for the selected quality.")
+        if st.button("Download Media"):
+            try:
+                if "MP3" in download_format:
+                    audio_stream = yt.streams.filter(only_audio=True).first()
+                    audio_data = io.BytesIO()
+                    audio_stream.stream_to_buffer(audio_data)
+                    audio_data.seek(0)
+                    st.info(f"Preparing download of {yt.title}.mp3...")
+                    st.download_button(label=f"Click to Download {yt.title}.mp3", key=f"{yt.title}.mp3", data=audio_data, file_name=f"{yt.title}.mp3")
+                else:
+                    video_stream = yt.streams.filter(progressive=True, file_extension="mp4").first()
+                    video_data = io.BytesIO()
+                    video_stream.stream_to_buffer(video_data)
+                    video_data.seek(0)
+                    st.info(f"Preparing download of {yt.title}.mp4...")
+                    st.download_button(label=f"Click to Download {yt.title}.mp4", key=f"{yt.title}.mp4", data=video_data, file_name=f"{yt.title}.mp4")
+            except Exception as e:
+                st.error(f"An error occurred: {str(e)}")
 
 # Add a footer
 st.markdown("Made with ❤️ by Shubham Gupta")
