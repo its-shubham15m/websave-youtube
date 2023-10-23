ba301d9cec108605d250f4db574fb0ec6af2aabc
Shubham Gupta <114990707+its-shubham15m@users.noreply.github.com>
2023-10-24 01:11:53 +0530

    quality enhancement

--- youtube.py
+++ youtube.py
@@ -21,7 +21,7 @@ st.markdown(
         background-size: cover;
         background-position: center;
     }}
-
+    
     .thumbnail-container {{
         border-radius: 10px;
         overflow: hidden;
@@ -39,7 +39,6 @@ st.markdown(
 # Input field for the YouTube URL
 url = st.text_input("Enter the YouTube URL:")
 
-
 @st.cache_data
 def get_video_info(url):
     try:
@@ -50,12 +49,10 @@ def get_video_info(url):
         st.error(f"An error occurred: {e}")
         return None, None
 
-
 def resize_thumbnail(thumbnail_url):
     response = requests.get(thumbnail_url)
     img = Image.open(BytesIO(response.content))
     return img
-
 
 # Check if a URL is provided
 if url:
@@ -75,50 +72,28 @@ if url:
 
             # Generate a direct download link for the selected stream
             stream_options = [f"{stream.resolution} - {stream.mime_type} - {stream.itag}" for stream in video_streams]
-            selected_stream_option = st.selectbox("Select a video stream to generate a direct download link:",
-                                                  stream_options)
-
-            extension_choice = st.radio("Select extension:", ("mp4", "webm"))
-
-            # Generate quality choices dynamically based on available video streams
-            available_qualities = set([stream.resolution for stream in video_streams])
-            quality_choices = list(available_qualities)
-            quality_choice = st.selectbox("Select quality:", quality_choices)
-
+            selected_stream_option = st.selectbox("Select a video stream to generate a direct download link:", stream_options)
             if selected_stream_option:
                 selected_stream_index = stream_options.index(selected_stream_option)
                 selected_stream = video_streams[selected_stream_index]
                 download_url = selected_stream.url
-                file_extension = extension_choice
-                quality = quality_choice
-                file_name = f"video_{quality}.{file_extension}"
                 st.subheader("Download Video:")
-                st.markdown(f'<a href="{download_url}" download="{file_name}">Click to Download</a>',
-                            unsafe_allow_html=True)
+                st.markdown(f'<a href="{download_url}" download>Click to Download</a>', unsafe_allow_html=True)
 
         if download_option == "Audio":
             audio_streams = yt.streams.filter(only_audio=True, file_extension='mp4')
 
             # Generate audio quality choices dynamically
             audio_quality_choices = [f"{audio_stream.abr.replace('kbps', '')}kbps" for audio_stream in audio_streams]
-
-            extension_choice = st.radio("Select extension:", ("mp3", "m4a"))
-
-            # Generate quality choices dynamically based on available audio streams
-            available_audio_qualities = set([audio_stream.abr for audio_stream in audio_streams])
-            audio_quality_choices = list(available_audio_qualities)
             audio_quality = st.selectbox("Select audio quality:", audio_quality_choices)
 
             if st.button("Download Audio"):
-                selected_audio_stream = next(
-                    (audio_stream for audio_stream in audio_streams if audio_quality in audio_stream.abr), None)
+                # Find the audio stream for the selected quality
+                selected_audio_stream = next((audio_stream for audio_stream in audio_streams if audio_quality in audio_stream.abr), None)
                 if selected_audio_stream:
                     download_url = selected_audio_stream.url
-                    file_extension = extension_choice
-                    file_name = f"audio_{audio_quality}.{file_extension}"
                     st.subheader("Download Audio:")
-                    st.markdown(f'<a href="{download_url}" download="{file_name}">Click to Download</a>',
-                                unsafe_allow_html=True)
+                    st.markdown(f'<a href="{download_url}" download>Click to Download</a>', unsafe_allow_html=True)
                 else:
                     st.warning("No audio stream available for the selected quality.")
 
